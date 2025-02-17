a4377df9198fe1fc8665340f92e68d1e25ac4778
Najish Ali <sayyednajishali@gmail.com>
2025-02-17 12:50:09 +0530

    Modify server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const os = require("os");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Health check route
+// Health check route of app
 app.get("/health", (req, res) => {
   res.json({ status: "UP", server: os.hostname() });
 });
 
-// Main route
+// Main route of app
 app.get("/", (req, res) => {
   res.send(`Node.js app is running on port ${PORT}`);
 });
 
-// Start server
+// Start server of app
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
